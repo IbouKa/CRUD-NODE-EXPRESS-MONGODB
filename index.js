23e765eac750ef0a4cd6a8c531fc09b4ac49a502
ibou-ka <ibou.ka@terangacloud.com>
2024-04-17 00:47:16 +0000

    secure

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(() => console.log('Connected to MongoDB'))
+.then(() => console.log('Connected to Mongodb'))
 .catch(err => console.error('Error connecting to MongoDB:', err));
